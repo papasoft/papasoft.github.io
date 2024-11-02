e7bc8ddcaf82dd9f6cfa360ec6ac790a090d081b
Rick Tuttle <rick@papasoft.com>
2024-11-02 15:45:33 -0400

    Github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
+		site: 'https://papasoft.github.io',
+	  integrations: [
 		starlight({
 			title: 'Papasoft',
 			logo: {
