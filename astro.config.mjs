02f439de96c96294aadb7d9544715f7eb3aa7746
Rick Tuttle <rick@papasoft.com>
2025-03-31 10:42:22 -0400

    Revert "Github pages setup"
    This reverts commit e7bc8ddcaf82dd9f6cfa360ec6ac790a090d081b.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-		site: 'https://papasoft.github.io',
-	  integrations: [
+	integrations: [
 		starlight({
 			title: 'Papasoft',
 			logo: {
